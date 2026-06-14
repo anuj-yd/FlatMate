c3498a8f3bb4e13b84a744feec74abdb885f78cb
Anuj Yadav <150239759+anuj-yd@users.noreply.github.com>
2026-06-14 18:03:10 +0530

    Prisam settup done

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
+require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server started on port 3000');
 });
-
